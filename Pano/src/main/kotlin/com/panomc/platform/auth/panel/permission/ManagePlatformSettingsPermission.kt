6acc39b692f30f18e9f92dd4e0f9257a02f4b48d
kahverengi <ahmet@duruer.dev>
2025-02-09 23:45:04 +0300

    fix: fix permissions are not getting initialized

--- Pano/src/main/kotlin/com/panomc/platform/auth/panel/permission/ManagePlatformSettingsPermission.kt
+++ Pano/src/main/kotlin/com/panomc/platform/auth/panel/permission/ManagePlatformSettingsPermission.kt
@@ -1,5 +1,7 @@
 package com.panomc.platform.auth.panel.permission
 
+import com.panomc.platform.annotation.Permission
 import com.panomc.platform.auth.PanelPermission
 
+@Permission
 class ManagePlatformSettingsPermission : PanelPermission("fa-cog")

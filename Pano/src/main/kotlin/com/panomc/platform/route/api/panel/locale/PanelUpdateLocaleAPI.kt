da897e033a4985c42c9dcf96199d6fc40a783aa0
kahverengi <ahmet@duruer.dev>
2025-07-07 23:54:15 +0300

    fix: fix derivatives input is not correctly handled in panel update locale endpoint

--- Pano/src/main/kotlin/com/panomc/platform/route/api/panel/locale/PanelUpdateLocaleAPI.kt
+++ Pano/src/main/kotlin/com/panomc/platform/route/api/panel/locale/PanelUpdateLocaleAPI.kt
@@ -51,7 +51,7 @@ class PanelUpdateLocaleAPI(
         val code = data.getString("code")
         val name = data.getString("name")
         val dateFnsCode = data.getString("dateFnsCode")
-        val derivatives = data.getJsonArray("dateFnsCode").map { it.toString() }
+        val derivatives = data.getJsonArray("derivatives").map { it.toString() }
 
         validateInput(code, name, dateFnsCode, derivatives)
 

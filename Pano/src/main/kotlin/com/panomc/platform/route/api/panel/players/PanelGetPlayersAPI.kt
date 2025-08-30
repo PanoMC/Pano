d94eee4741de1cdf4f81b29cc4cd3d54e560d0a8
kahverengi <ahmet@duruer.dev>
2025-08-30 19:05:04 +0300

    refactor: refactor panel get players

--- Pano/src/main/kotlin/com/panomc/platform/route/api/panel/players/PanelGetPlayersAPI.kt
+++ Pano/src/main/kotlin/com/panomc/platform/route/api/panel/players/PanelGetPlayersAPI.kt
@@ -4,23 +4,25 @@ package com.panomc.platform.route.api.panel.players
 import com.panomc.platform.annotation.Endpoint
 import com.panomc.platform.auth.AuthProvider
 import com.panomc.platform.auth.panel.permission.ManagePlayersPermission
-import com.panomc.platform.model.PanelApi
-import com.panomc.platform.model.Path
-import com.panomc.platform.model.Result
-import com.panomc.platform.model.RouteType
-import com.panomc.platform.service.PlayerService
+import com.panomc.platform.db.DatabaseManager
+import com.panomc.platform.db.model.PermissionGroup
+import com.panomc.platform.error.NotExists
+import com.panomc.platform.error.PageNotFound
+import com.panomc.platform.model.*
 import com.panomc.platform.util.PlayerStatus
+import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.validation.ValidationHandler
 import io.vertx.ext.web.validation.builder.Parameters.optionalParam
 import io.vertx.ext.web.validation.builder.ValidationHandlerBuilder
 import io.vertx.json.schema.SchemaRepository
 import io.vertx.json.schema.common.dsl.Schemas.*
+import kotlin.math.ceil
 
 @Endpoint
 class PanelGetPlayersAPI(
     private val authProvider: AuthProvider,
-    private val playerService: PlayerService
+    private val databaseManager: DatabaseManager
 ) : PanelApi() {
     override val paths = listOf(Path("/api/panel/players", RouteType.GET))
 
@@ -49,6 +51,85 @@ class PanelGetPlayersAPI(
         val page = parameters.queryParameter("page")?.long ?: 1L
         val permissionGroupName = parameters.queryParameter("permissionGroup")?.string
 
-        return playerService.getPlayers(playerStatus, page, permissionGroupName)
+        val sqlClient = databaseManager.getSqlClient()
+
+        var permissionGroup: PermissionGroup? = null
+
+        if (permissionGroupName != null && permissionGroupName != "-") {
+            val isTherePermission =
+                databaseManager.permissionGroupDao.isThereByName(permissionGroupName, sqlClient)
+
+            if (!isTherePermission) {
+                throw NotExists()
+            }
+
+            val permissionGroupId =
+                databaseManager.permissionGroupDao.getPermissionGroupIdByName(permissionGroupName, sqlClient)!!
+
+            permissionGroup = PermissionGroup(permissionGroupId, permissionGroupName)
+        }
+
+        if (permissionGroupName != null && permissionGroupName == "-") {
+            permissionGroup = PermissionGroup(name = "-")
+        }
+
+        val count =
+            if (permissionGroup != null)
+                databaseManager.userDao.getCountOfUsersByPermissionGroupId(permissionGroup.id, sqlClient)
+            else
+                databaseManager.userDao.countByStatus(playerStatus, sqlClient)
+
+        var totalPage = ceil(count.toDouble() / 10).toLong()
+
+        if (totalPage < 1)
+            totalPage = 1
+
+        if (page > totalPage || page < 1) {
+            throw PageNotFound()
+        }
+
+        val userList =
+            if (permissionGroup != null)
+                databaseManager.userDao.getAllByPageAndPermissionGroup(page, permissionGroup.id, sqlClient)
+            else
+                databaseManager.userDao.getAllByPageAndStatus(page, playerStatus, sqlClient)
+
+
+        val result = mutableMapOf<String, Any?>(
+            "playerCount" to count,
+            "totalPage" to totalPage
+        )
+
+        if (permissionGroup != null) {
+            result["permissionGroup"] = permissionGroup
+        }
+
+        if (userList.isEmpty()) {
+            return Successful(result)
+        }
+
+        val permissionGroupIdList = userList.map { it.permissionGroupId }
+        val userIdList = userList.map { it.id }
+        val usernameList = userList.map { it.username }
+        val permissions = databaseManager.permissionGroupDao.byListOfId(permissionGroupIdList, sqlClient)
+        val userIdTicketCountMap = databaseManager.ticketDao.countByUserIdList(userIdList, sqlClient)
+        val usernameInGameMap = databaseManager.serverPlayerDao.existsByUsernameList(usernameList, sqlClient)
+
+        result["players"] = userList.map {
+            val user = JsonObject.mapFrom(it)
+
+            user.put("isBanned", it.banned)
+            user.put("inGame", usernameInGameMap[it.username])
+            user.put("permissionGroup", permissions[it.permissionGroupId]?.name ?: "-")
+            user.put("ticketCount", userIdTicketCountMap[it.id])
+            user.put("isEmailVerified", it.emailVerified)
+
+            user.remove("password")
+            user.remove("banned")
+
+            user
+        }
+
+        return Successful(result)
     }
 }
--- Pano/src/main/kotlin/com/panomc/platform/service/PlayerService.kt
+++
@@ -1,8 +0,0 @@
-package com.panomc.platform.service
-
-import com.panomc.platform.model.Result
-import com.panomc.platform.util.PlayerStatus
-
-interface PlayerService {
-    suspend fun getPlayers(playerStatus: PlayerStatus, page: Long, permissionGroupName: String?): Result
-}

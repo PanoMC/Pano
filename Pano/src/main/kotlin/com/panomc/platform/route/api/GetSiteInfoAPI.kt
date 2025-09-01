905a850c01bd0fb15fa37e138b1d4f1615b3ef3e
kahverengi <ahmet@duruer.dev>
2025-09-02 02:01:08 +0300

    feat: added theme settings config system

--- Pano/src/main/kotlin/com/panomc/platform/route/api/GetSiteInfoAPI.kt
+++ Pano/src/main/kotlin/com/panomc/platform/route/api/GetSiteInfoAPI.kt
@@ -4,10 +4,14 @@ import com.panomc.platform.AppConstants.DEFAULT_WEBSITE_LOGO_FILE
 import com.panomc.platform.Main.Companion.VERSION
 import com.panomc.platform.PluginManager
 import com.panomc.platform.PluginUiManager
+import com.panomc.platform.UIManager
 import com.panomc.platform.annotation.Endpoint
 import com.panomc.platform.config.ConfigManager
+import com.panomc.platform.db.DatabaseManager
 import com.panomc.platform.model.*
+import com.panomc.platform.route.api.panel.theme.PanelUpdateThemeSettingsAPI.Companion.THEME_SETTINGS
 import com.panomc.platform.util.HashUtil.hash
+import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.RoutingContext
 import io.vertx.json.schema.SchemaRepository
 import java.io.File
@@ -16,14 +20,16 @@ import java.io.File
 class GetSiteInfoAPI(
     private val configManager: ConfigManager,
     private val pluginManager: PluginManager,
-    private val pluginUiManager: PluginUiManager
+    private val pluginUiManager: PluginUiManager,
+    private val databaseManager: DatabaseManager,
+    private val uiManager: UIManager
 ) : Api() {
     override val paths = listOf(Path("/api/siteInfo", RouteType.GET))
 
     override fun getValidationHandler(schemaRepository: SchemaRepository) = null
 
     override suspend fun handle(context: RoutingContext): Result {
-        val response = mutableMapOf<String, Any>()
+        val response = mutableMapOf<String, Any?>()
         val config = configManager.config
 
         val websiteLogoPath =
@@ -48,6 +54,21 @@ class GetSiteInfoAPI(
         }
 
         response["emailEnabled"] = config.email.enabled
+
+        val sqlClient = databaseManager.getSqlClient()
+        val themeSettingsProperty = databaseManager.systemPropertyDao.getByOption(THEME_SETTINGS, sqlClient)
+
+        var themeSettings = JsonObject()
+
+        if (themeSettingsProperty != null) {
+            val currentThemeSettings = JsonObject(themeSettingsProperty.value).getJsonObject(uiManager.activeTheme)
+
+            if (currentThemeSettings != null) {
+                themeSettings = currentThemeSettings
+            }
+        }
+
+        response["themeSettings"] = themeSettings
 
         return Successful(response)
     }
---
+++ Pano/src/main/kotlin/com/panomc/platform/route/api/panel/theme/PanelUpdateThemeSettingsAPI.kt
@@ -0,0 +1,201 @@
+package com.panomc.platform.route.api.panel.theme
+
+import com.panomc.platform.UIManager
+import com.panomc.platform.annotation.Endpoint
+import com.panomc.platform.auth.AuthProvider
+import com.panomc.platform.auth.panel.permission.ManageViewPermission
+import com.panomc.platform.config.ConfigManager
+import com.panomc.platform.db.DatabaseManager
+import com.panomc.platform.db.model.SystemProperty
+import com.panomc.platform.error.NoPermission
+import com.panomc.platform.model.*
+import io.vertx.core.Handler
+import io.vertx.core.json.JsonArray
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.web.RoutingContext
+import io.vertx.ext.web.handler.BodyHandler
+import io.vertx.ext.web.validation.RequestPredicate
+import io.vertx.ext.web.validation.ValidationHandler
+import io.vertx.ext.web.validation.builder.Bodies
+import io.vertx.ext.web.validation.builder.ValidationHandlerBuilder
+import io.vertx.json.schema.SchemaRepository
+import io.vertx.json.schema.common.dsl.Schemas.objectSchema
+import java.io.File
+import java.util.*
+
+@Endpoint
+class PanelUpdateThemeSettingsAPI(
+    private val configManager: ConfigManager,
+    private val authProvider: AuthProvider,
+    private val databaseManager: DatabaseManager,
+    private val uiManager: UIManager
+) : PanelApi() {
+    override val paths = listOf(Path("/api/panel/theme/settings", RouteType.PUT))
+
+    private val themeSettingsFileUploadPath = "theme-settings"
+
+    companion object {
+        const val THEME_SETTINGS = "theme_settings"
+    }
+
+    override fun bodyHandler(): Handler<RoutingContext> =
+        BodyHandler.create()
+            .setDeleteUploadedFilesOnEnd(true)
+            .setBodyLimit(100 * 1024 * 1024) // 100MB
+
+    override fun getValidationHandler(schemaRepository: SchemaRepository): ValidationHandler =
+        ValidationHandlerBuilder.create(schemaRepository)
+            .body(
+                Bodies.multipartFormData(
+                    objectSchema()
+                        .requiredProperty(
+                            "settings", objectSchema()
+                        )
+                )
+            )
+            .predicate(RequestPredicate.BODY_REQUIRED)
+            .build()
+
+    override suspend fun handle(context: RoutingContext): Result {
+        authProvider.requirePermission(ManageViewPermission(), context)
+
+        if (!uiManager.activatedUIList.containsKey(Type.THEME_UI)) {
+            throw NoPermission()
+        }
+
+        val parameters = getParameters(context)
+        val data = parameters.body().jsonObject
+
+        val fileUploads = context.fileUploads()
+
+        val newSettings = data.getJsonObject("settings")
+
+        val folder = configManager.config
+            .fileUploadsFolder + File.separator + themeSettingsFileUploadPath + File.separator
+        val folderFile = File(folder)
+
+        if (!folderFile.exists() || !folderFile.isDirectory) {
+            folderFile.deleteRecursively()
+            folderFile.mkdirs()
+        }
+
+        val newFiles = mutableMapOf<String, MutableList<String>>()
+
+        if (fileUploads.isNotEmpty()) {
+            val fileUploadsByName = fileUploads.groupBy { it.name() }
+
+            fileUploadsByName.forEach {
+                it.value.forEach { uploadedFile ->
+                    val containsExt = uploadedFile.uploadedFileName().contains(".")
+                    val split = uploadedFile.uploadedFileName().split(".")
+
+                    val newFileName = UUID.randomUUID().toString() + if (containsExt) "." + split.last() else ""
+                    val newPath = folder + newFileName
+                    val uploadedFileObj = File(uploadedFile.uploadedFileName())
+
+                    uploadedFileObj.copyTo(File(newPath), true)
+                    uploadedFileObj.delete()
+
+                    if (newFiles[it.key] == null) {
+                        newFiles[it.key] = mutableListOf()
+                    }
+
+                    newFiles[it.key]!!.add(newFileName)
+                }
+            }
+
+            val newSettingsFiles = newSettings.getJsonObject("files") ?: JsonObject()
+
+            newFiles.forEach {
+                var list = newSettingsFiles.getJsonArray(it.key)
+
+                if (list == null) {
+                    val jsonArray = JsonArray()
+                    newSettingsFiles.put(it.key, jsonArray)
+                    list = jsonArray
+                }
+
+                it.value.forEach { fileName ->
+                    list.add(fileName)
+                }
+            }
+
+            newSettings.put("files", newSettingsFiles)
+        }
+
+        val removeFiles = newSettings.getJsonArray("remove-files")
+
+        if (removeFiles != null && !removeFiles.isEmpty) {
+            val newSettingsFiles = newSettings.getJsonObject("files")
+
+            removeFiles.forEach { fileName ->
+                val file = File(folder + fileName)
+
+                if (file.exists()) {
+                    file.deleteRecursively()
+                }
+
+                if (newSettingsFiles != null) {
+                    newSettingsFiles.forEach {
+                        val list = it.value as JsonArray
+
+                        if (list.contains(fileName)) {
+                            list.remove(fileName)
+                        }
+
+                        if (list.isEmpty) {
+                            newSettingsFiles.remove(it.toString())
+                        }
+                    }
+                }
+            }
+
+            newSettings.remove("remove-files")
+        }
+
+        val sqlClient = getSqlClient()
+
+        val existingSettingsProperty = databaseManager.systemPropertyDao.getByOption(THEME_SETTINGS, sqlClient)
+
+        if (existingSettingsProperty != null) {
+            val settingsProperty = JsonObject(existingSettingsProperty.value)
+            val currentThemeSettings = settingsProperty.getJsonObject(uiManager.activeTheme)
+
+            if (currentThemeSettings != null) {
+                val existingFiles = currentThemeSettings.getJsonObject("files")
+
+                if (existingFiles != null) {
+                    val newSettingsFiles = newSettings.getJsonObject("files") ?: JsonObject()
+
+                    existingFiles.filter { !(newSettingsFiles.getJsonArray(it.key) ?: JsonArray()).contains(it.value) }
+                        .forEach {
+                            val file = File(folder + it)
+
+                            if (file.exists()) {
+                                file.deleteRecursively()
+                            }
+                        }
+                }
+            }
+
+            settingsProperty.put(uiManager.activeTheme, newSettings)
+
+            databaseManager.systemPropertyDao.update(THEME_SETTINGS, settingsProperty.encode(), sqlClient)
+
+            return Successful(newSettings.map)
+        }
+
+        val newSettingsProperty = JsonObject()
+
+        newSettingsProperty.put(uiManager.activeTheme, newSettings)
+
+        databaseManager.systemPropertyDao.add(
+            SystemProperty(
+                option = THEME_SETTINGS,
+                value = newSettingsProperty.encode()
+            ), sqlClient
+        )
+
+        return Successful(newSettings.map)
+    }
+}

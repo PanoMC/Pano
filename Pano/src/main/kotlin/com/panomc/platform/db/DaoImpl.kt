b73dcc5b4bad540fc44af8973db04546a133e544
kahverengi <ahmet@duruer.dev>
2025-10-26 15:07:32 +0300

    refactor: removed some useless class

--- Pano/src/main/kotlin/com/panomc/platform/db/DaoImpl.kt
+++
@@ -1,6 +0,0 @@
-package com.panomc.platform.db
-
-abstract class DaoImpl(val databaseManager: DatabaseManager, val tableName: String) {
-
-    fun getTablePrefix(): String = databaseManager.getTablePrefix()
-}

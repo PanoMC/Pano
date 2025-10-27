e6162b0f7be75e241d164600dc9f3e37ffba84f6
kahverengi <ahmet@duruer.dev>
2025-10-27 15:07:39 +0300

    feat: added player authentication event support

---
+++ Pano/src/main/kotlin/com/panomc/platform/server/event/PlayerAuthenticateEvent.kt
@@ -0,0 +1,34 @@
+package com.panomc.platform.server.event
+
+import com.panomc.platform.annotation.Event
+import com.panomc.platform.auth.AuthProvider
+import com.panomc.platform.db.DatabaseManager
+import com.panomc.platform.db.model.Server
+import com.panomc.platform.model.Error
+import com.panomc.platform.server.ServerEvent
+import com.panomc.platform.server.ServerManager
+import com.panomc.platform.server.event.request.PlayerAuthenticateEventRequest
+import com.panomc.platform.server.response.PlayerAuthenticateEventResponse
+
+@Event
+class PlayerAuthenticateEvent(
+    private val serverManager: ServerManager,
+    private val authProvider: AuthProvider,
+    private val databaseManager: DatabaseManager
+) : ServerEvent<PlayerAuthenticateEventRequest>() {
+    override suspend fun handle(request: PlayerAuthenticateEventRequest, server: Server) {
+        val sqlClient = databaseManager.getSqlClient()
+
+        var success = true
+
+        try {
+            authProvider.authenticate(request.username, request.password, sqlClient)
+        } catch (_: Error) {
+            success = false
+        }
+
+        val message = PlayerAuthenticateEventResponse(request.eventId, success)
+
+        serverManager.sendMessage(message, server)
+    }
+}

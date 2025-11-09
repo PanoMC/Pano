ce00a01e788d78454ddf6f46e3f0e1302b19de20
kahverengi <ahmet@duruer.dev>
2025-11-09 23:30:46 +0300

    fix: fixed wrong naming in get player info event response

--- Pano/src/main/kotlin/com/panomc/platform/server/response/GetPlayerInfoEventResponse.kt
+++ Pano/src/main/kotlin/com/panomc/platform/server/response/GetPlayerInfoEventResponse.kt
@@ -6,6 +6,6 @@ data class GetPlayerInfoEventResponse(
     val registered: Boolean,
     val banned: Boolean,
     val verified: Boolean,
-    val pendingEmail: String? = null,
+    val email: String? = null,
     val locale: String? = null,
 ) : ServerEventResponse()
